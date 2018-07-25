d1ea0fa2a3dc04f5f2bded8267ef11d3a8ecc341
Ringo <ringong95@gmail.com>
2018-07-25 13:05:51 -0700

    Added extra layer of data checking.

--- app/reducers/queriedData.js
+++ app/reducers/queriedData.js
@@ -5,7 +5,13 @@ import { LOADDATA } from '../actions/queriedData';
 export default (state: array =[], action: actionType) => {
   switch (action.type) {
     case LOADDATA:
-    console.log(action.payload)
+      console.log('test',   action.payload.data.filter((eachData) => {
+        if (eachData.dateToContact >= new Date()){
+          return eachData
+        }
+        return false
+      }))
+      
       return  action.payload.data;
     default:
       return state;
